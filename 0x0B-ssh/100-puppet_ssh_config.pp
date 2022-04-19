1ec265a88775d95a3eb4649675238c600a6e4226
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-04-19 12:39:05 -0700

    testing

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 #comment
 exec { 'echo':
   command => '/bin/echo "PasswordAuthentication no" >> /etc/ssh/ssh_config',}
-exec { 'echo':
+exec { 'echo22':
   command => '/bin/echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',}
