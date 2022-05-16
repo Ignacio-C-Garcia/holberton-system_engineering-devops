4c78961b705f8f736179ca30d86d14a2eb3a47b9
Ignacio Castellan <ignacioacgarcia@gmail.com>
2022-05-16 14:35:04 -0700

    csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""this is a comment"""
+import requests
+from sys import argv
+import csv
+
+if __name__ == '__main__':
+
+    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}"
+    list_todo = requests.get(todo_url.format(argv[1])).json()
+    usr_url = "https://jsonplaceholder.typicode.com/users/{}"
+    user = requests.get(usr_url.format(argv[1])).json()
+    user_name = user.get('name')
+    task_completed = 0
+    task_titles = ''
+
+    for todo in list_todo:
+        if todo.get('completed') is True:
+            task_completed += 1
+            task_titles += '\t ' + todo.get('title') + '\n'
+
+    f_txt = 'Employee {} is done with tasks({}/{}):'
+    print(f_txt.format(user_name, task_completed, len(list_todo)))
+    print(task_titles, end='')
+
+    with open('{}.csv'.format(argv[1]), 'w', newline='') as csvfile:
+        spamwriter = csv.writer(
+                csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        for todo in list_todo:
+            user_id = todo.get('userId')
+            task_completed_status = todo.get('completed')
+            task_title = todo.get('title')
+            username = user.get('username')
+            spamwriter.writerow([user_id, username, task_completed_status, task_title])
